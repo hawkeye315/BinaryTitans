383edf1d54cfe3622564ef90c6f553c1a302071c
Nicholas Sparks <nscelica@yahoo.com>
2015-06-21 14:06:22 -0400

    Some more tweaks to HUD and cleanup

---
+++ 2D Platformer Prototype/Assets/Scripts/HUD.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+	private GameManager gameManager;
+
+	Text txt;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = FindObjectOfType<GameManager>();
+		txt = gameObject.GetComponent<Text>(); 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		txt.text="Lives : " + gameManager.getLives() + "\nScore : " + gameManager.getScore() + "\nHealth : " + gameManager.getHealth();
+	}
+}
