a54c28229031ddab4f65669f4dbf833562add8eb
Nicholas Sparks <nscelica@yahoo.com>
2015-06-22 07:33:52 -0400

    Added random jump interval for EnemyAI

--- 2D Platformer Prototype/Assets/Scripts/EnemyAI.cs
+++ 2D Platformer Prototype/Assets/Scripts/EnemyAI.cs
@@ -8,6 +8,8 @@ public class EnemyAI : MonoBehaviour {
 	public float moveDistance;
 
 	private Vector3 enemyStartPosition;
+	private double lastInterval; // Last interval end time
+	private double nextInterval;
 
 	// Random jump height
 	public float jumpHeight;
@@ -19,6 +21,8 @@ public class EnemyAI : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		lastInterval = Time.time;
+		nextInterval = Time.time + (Random.value * 5 + 3);
 		enemyStartPosition = transform.position;
 		moveDirection = 1;
 	}
@@ -26,10 +30,18 @@ public class EnemyAI : MonoBehaviour {
 	// Update is called once per frame
 		// Update is called once per frame
 	void Update () {
+		if (Time.time >= nextInterval) {
+			Jump ();
+			nextInterval = Time.time + (Random.value * 5 + 3);
+		}
 		if (transform.position.x <= enemyStartPosition.x)
 			moveDirection = 1;
 		else if (transform.position.x >= enemyStartPosition.x + moveDistance)
 			moveDirection = -1;
 		GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * moveDirection, GetComponent<Rigidbody2D>().velocity.y);
+	}
+	public void Jump()
+	{
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);  
 	}
 }
