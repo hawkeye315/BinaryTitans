7d8a814aca670a5818820b9a2b451192bd66dc3f
Jonas Wedin <jonas@lechuck.se>
2015-06-25 00:02:14 +0200

    Started player class with enum states

---
+++ 2D Platformer Prototype/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Fields
+	// Do not set these in the editor, they change anyway
+	public int health;
+	public int lives;
+	// These are fine to change
+	public float moveSpeed;
+	public float jumpHeight;
+	// Private variables
+	private float moveVelocity;
+	private State currentState;
+
+	private Animator anim;
+
+	public enum ControlKeys {
+		Jump = KeyCode.Space,
+		Move_Left = KeyCode.A,
+		Move_Right = KeyCode.D,
+		Attack = KeyCode.P
+	}
+
+	public enum State {
+		Idle,
+		Jumping,
+		DoubleJumping,
+		Moving
+	}
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+		lives = 3;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
