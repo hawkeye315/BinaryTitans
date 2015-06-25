9c1d464796f6dd7ce3327f731dce04d54049fcdb
Jonas Wedin <jonas@lechuck.se>
2015-06-25 19:45:52 +0200

    removed enum and replaced with dictionary

--- 2D Platformer Prototype/Assets/Scripts/Player.cs
+++ 2D Platformer Prototype/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
@@ -13,15 +14,18 @@ public class Player : MonoBehaviour {
 	// Private variables
 	private float moveVelocity;
 	private State currentState;
+	// Ground check gameobject under Player
+	public Transform groundCheck;
+	// Ground Layer
+	public LayerMask whatIsGround;
+	// Amount of space to check to see if touching the ground
+	public float groundCheckRadius;
+	// Is the player touching the ground?
+	private bool grounded;
+	// Is the player moving?
+	private bool moving;
 
 	private Animator anim;
-
-	public enum ControlKeys {
-		Jump = KeyCode.Space,
-		Move_Left = KeyCode.A,
-		Move_Right = KeyCode.D,
-		Attack = KeyCode.P
-	}
 
 	public enum State {
 		Idle,
@@ -30,22 +34,41 @@ public class Player : MonoBehaviour {
 		Moving
 	}
 
+	public Dictionary <string, KeyCode> controlKeys;
+
 	// Use this for initialization
 	void Start () {
 		health = 100;
 		lives = 3;
-
-	
+		//set up keys
+		controlKeys.Add("moveLeft", KeyCode.A);
+		controlKeys.Add("moveRight", KeyCode.D);
+		controlKeys.Add("jump", KeyCode.Space);
+		controlKeys.Add("attack", KeyCode.P);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		switch(currentState){
+		switch(currentState)
+		{
 			case State.Idle:
 				//From here we can go to moving, or jumping 
+				if(Input.GetKeyDown(controlKeys["moveLeft"])){
+					MovePlayerLeft();
+					currentState = State.Moving;
+				}
+				if(Input.GetKeyDown(controlKeys["moveRight"])){
+					MovePlayerRight();
+					currentState = State.Moving;
+				}
+				if(Input.GetKeyDown(controlKeys["jump"])){
+					Jump();
+					currentState = State.Jumping;
+				}
 				break;
 			case State.Jumping:
 				//From here we can go to double jumping, idle, moving
+				//if(grounded)
 				break;
 			case State.DoubleJumping:
 				//From here we can go to idle, moving
@@ -55,4 +78,33 @@ public class Player : MonoBehaviour {
 				break;
 		}
 	}
+
+	// This function checks to see if the Player is touching the ground and
+	// sets the ground variable to true if he is.
+	private void FixedUpdate()
+	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+		moving = (GetComponent<Rigidbody2D>().velocity.x != 0);
+	}
+
+	/*
+	 * Help methods
+	 */
+
+	private void MovePlayerLeft(){
+		MovePlayer(moveSpeed, -1, GetComponent<Rigidbody2D>().velocity.y, 1);
+	}
+
+	private void MovePlayerRight(){
+		MovePlayer(moveSpeed, 1, GetComponent<Rigidbody2D>().velocity.y, 1);
+	}
+
+	private void MovePlayer(float xMoveSpeed, int xDirection, float yMoveSpeed, int yDirection) {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(xMoveSpeed * xDirection, yMoveSpeed * yDirection);
+	}
+
+	private void Jump()
+	{
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);  
+	}    
 }
