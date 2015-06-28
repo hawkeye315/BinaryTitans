4bf2d5b6ee8bd30725e19f6cd6b2ce9349ef2412
Nicholas Sparks <nscelica@yahoo.com>
2015-06-28 12:45:23 -0400

    added LevelBuilder

---
+++ 2D Platformer Prototype/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+	private int[] floor = {0,0,0,0,0,0,1,1,1,1,2,2,2,2,-1,-1,0,0,0};
+	
+	void Start() {
+		for (int x = 0; x < floor.Length; x++) {
+			if (floor[0] >= 0)
+				Instantiate(Resources.Load("Floor1_1x4"), new Vector3(x*4, floor[x], 2), Quaternion.Euler(new Vector3(270,0,0)));
+		}
+	}
+}
