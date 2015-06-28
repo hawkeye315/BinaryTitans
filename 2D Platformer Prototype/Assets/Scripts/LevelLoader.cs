e41f0608d2605beb191503eea36dc5623f3f0593
Nicholas Sparks <nscelica@yahoo.com>
2015-06-28 12:54:02 -0400

    another update

--- 2D Platformer Prototype/Assets/Scripts/LevelLoader.cs
+++ 2D Platformer Prototype/Assets/Scripts/LevelLoader.cs
@@ -8,6 +8,8 @@ public class LevelLoader : MonoBehaviour {
 		for (int x = 0; x < floor.Length; x++) {
 			if (floor[x] >= 0)
 				Instantiate(Resources.Load("Floor1_1x4"), new Vector3(x*4, floor[x], 2), Quaternion.Euler(new Vector3(270,0,0)));
+			for (int y=-2; y<4; y++)
+				Instantiate(Resources.Load("Wall1_4x4"), new Vector3(x*4, y*4, 2), Quaternion.Euler(new Vector3(270,0,0)));
 		}
 	}
 }
