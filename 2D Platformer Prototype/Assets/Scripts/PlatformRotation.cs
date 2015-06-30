ae223c8d70c6efe11021aa8ed816cab5bcc774f7
Nicholas Sparks <nscelica@yahoo.com>
2015-06-30 01:04:54 -0400

    added rotating platform

---
+++ 2D Platformer Prototype/Assets/Scripts/PlatformRotation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformRotation : MonoBehaviour {
+	public float angle;
+	void Update () {
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,0), Time.deltaTime * 2);
+	}
+}
