d5ee3a8856dec3c2c4b62c1572700f6c56734c29
Jonas Wedin <jonas@lechuck.se>
2015-07-06 22:21:25 +0200

    Added options panel and animated the buttons

--- 2D Platformer Prototype/Assets/Scripts/UiManager.cs
+++ 2D Platformer Prototype/Assets/Scripts/UiManager.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class UiManager : MonoBehaviour {
 
+	public Animator MainButtonGroupAnimator;
+	public Animator OptionsGroupAnimator;
+
 	//Connected to BtnStartGame
 	public void StartGame(){
 		Application.LoadLevel("Level1");
@@ -11,5 +14,16 @@ public class UiManager : MonoBehaviour {
 	//Connected to BtnQuit
 	public void QuitGame(){
 		Application.Quit();
+	}
+
+	//Connected to BtnOptions
+	public void OpenOptions(){
+		MainButtonGroupAnimator.SetBool("MainButtonsIsHidden", true);
+		OptionsGroupAnimator.SetBool("OptionsIsHidden", false);
+	}
+
+	public void BackFromOptions(){
+		OptionsGroupAnimator.SetBool("OptionsIsHidden", true);
+		MainButtonGroupAnimator.SetBool("MainButtonsIsHidden", false);
 	}
 }
