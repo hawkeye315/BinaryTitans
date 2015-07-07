dc589af32a41bd4d3ab55729e5720866c09eea1f
Nicholas Sparks <nsparks86@gmail.com>
2015-07-07 13:55:06 -0400

    Quick fixes/tweaks

--- 2D Platformer Prototype/Assets/Scripts/EnemyAI.cs
+++ 2D Platformer Prototype/Assets/Scripts/EnemyAI.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class EnemyAI : MonoBehaviour {
 
 	public float moveSpeed;
-	private double nextInterval; // Last interval end time
+	private double nextInterval, nextMoveInterval; // Last interval end time
 	private int moveDirection;
-	private bool grounded;
+	private bool grounded,trigger,move;
 	private int health;
 	private Animator anim;
 	public int enemyType; //0-ground, 1-flying, 2-boss;
@@ -16,10 +16,13 @@ public class EnemyAI : MonoBehaviour {
 
 
 	void Start () {
+		trigger = false;
+		move = true;
 		player = GameObject.FindObjectOfType<Player>().transform;
 		rangeX = maxX - minX;
 		rangeY = maxY - minY;
 		nextInterval = Time.time + (Random.value * 5 + 1);
+		nextMoveInterval = Time.time + (Random.value * 3 + 2);
 		moveDirection = 1;
 		anim = GetComponent<Animator>();
 		nextPosition = transform.position;
@@ -29,38 +32,50 @@ public class EnemyAI : MonoBehaviour {
 	}
 
 	void Update () {
+		Vector3 dir = player.position - transform.position;
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		if (Time.time >= nextInterval) {
+			trigger = true;
+			nextInterval = Time.time + (Random.value * 5 + 1);
+		}
+		if (Time.time >= nextMoveInterval) {
+			move = true;
+			nextMoveInterval = Time.time + (Random.value * 3 + 2);
+		}
+
 		switch (enemyType) {
 		case 0:
-			Vector3 dir = player.position - transform.position;
-//			dir = player.InverseTransformDirection(dir);
-			float angle2 = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 			if (!Physics.Raycast (new Vector3 (transform.position.x + 1, transform.position.y), downV3, 5)) {
 				moveDirection = -1;
 			}
 			if (!Physics.Raycast (new Vector3 (transform.position.x - 1, transform.position.y), downV3, 5)) {
 				moveDirection = 1;
 			}
-			if (Time.time >= nextInterval) {
+			if (trigger) {
 				Jump (Random.value * 5 + 5);
-				nextInterval = Time.time + (Random.value * 5 + 1);
 				if (Mathf.Abs(dir.x) <= 20)
-					Shoot (angle2);
+					Shoot (angle);
 			}
 			GetComponent<Rigidbody>().velocity = new Vector3(moveSpeed * moveDirection, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z);
 			break;
 		case 1:
-			if (Vector3.Distance(transform.position, nextPosition) <= 1){
-				nextPosition = new Vector3 (minX + Random.value * rangeX, minY + Random.value * rangeY, transform.position.z);
-				if(nextPosition.x - transform.position.x >= 0)
+//			if (Vector3.Distance(transform.position, nextPosition) <= 1){
+			if (move){
+					nextPosition = new Vector3 (minX + Random.value * rangeX, minY + Random.value * rangeY, transform.position.z);
+				if(player.position.x > transform.position.x)
 					moveDirection = 1;
 				else
 					moveDirection = -1;
 			}
 			transform.position = Vector3.Slerp (transform.position, nextPosition, Time.deltaTime * moveSpeed);
+			if (Mathf.Abs(dir.x) <= 20 && Mathf.Abs(dir.y) <= 20 && trigger)
+				Shoot (angle);
 			break;
 		case 2:
 			break;
 		}
+		trigger = false;
+		move = false;
 		if (moveDirection >= 0)
 			transform.eulerAngles = new Vector3(0,0,0);
 		else
