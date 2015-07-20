a2316a1dd736e430ff17a9ab105ec7777a8767c7
pcast01 <azrael7@gmail.com>
2015-07-19 23:39:12 -0500

    Added blaster weapon with Bullet Trail and Muzzle Flash.

--- 2D Platformer Prototype/Assets/Scripts/Weapon.cs
+++ 2D Platformer Prototype/Assets/Scripts/Weapon.cs
@@ -5,7 +5,11 @@ public class Weapon : MonoBehaviour {
 
     public float fireRate = 0;
     public float damage = 10;
-    public LayerMask whatToHit;
+
+    public Transform BulletTrailPrefab;
+    public Transform MuzzleFlashPrefab;
+    float timeToSpawnEffect = 0;
+    public float effectSpawnRate = 10;
 
     float timeToFire = 0;
     Transform firePoint;
@@ -21,7 +25,7 @@ public class Weapon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Shoot();
+        //Shoot();
         if (fireRate == 0)
         {
             if (Input.GetButtonDown("Fire1"))
@@ -39,36 +43,57 @@ public class Weapon : MonoBehaviour {
         }
 	}
 
-    void Shoot()
+   void Shoot()
     {
-        //Camera.main.WorldToScreenPoint
-        //Camera.main.ViewportToScreenPoint
-        //Camera.main.ScreenToWorldPoint
-        //Debug.Log("Shooting.");
         Vector3 mouseposZ = Input.mousePosition;
+        RaycastHit rHit;
         mouseposZ.z = 10;
         Vector3 mousePosition = new Vector3(Camera.main.ScreenToWorldPoint(mouseposZ).x, Camera.main.ScreenToWorldPoint(mouseposZ).y, Camera.main.ScreenToWorldPoint(mouseposZ).z);
-        //Debug.Log("mouseX: "+ mousePosition.x + " mouseY: " + mousePosition.y);
-        //Debug.Log("Input mouseX: " + Input.mousePosition.x + " mouseY: " + Input.mousePosition.y);
+
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
         Vector2 mousePos2 = new Vector2(mousePosition.x, mousePosition.y);
 
-        RaycastHit2D hitLC = Physics2D.Linecast(firePointPosition, mousePos2, whatToHit, 10);
-        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePos2 - firePointPosition, 100, whatToHit);
-        Debug.DrawLine(firePointPosition, (mousePos2 - firePointPosition) * 100, Color.cyan);
-        //Debug.Log("mousepos x: " + mousePos2.x + " y: " + mousePos2.y + " firepointPos x: " + firePointPosition.x + " y: " + firePointPosition.y);
-        //Debug.Log("whatToHit: " + whatToHit);
-        //Debug.Log("hit.collider = " + hit.collider);
-        if (hitLC.collider != null)
+        Ray shootRay = new Ray(firePointPosition, mousePos2);
+        Debug.DrawRay(firePointPosition, (mousePos2 - firePointPosition) * 100, Color.cyan);
+        float distance = 100f;
+       
+        Vector2 effectDirection = (mousePos2 - firePointPosition) * 100;
+        if (Time.time >= timeToSpawnEffect)
         {
-            Debug.Log("LC: " + hitLC.collider.name);
+            Effect();
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
         }
+        if (Physics.Raycast(shootRay, out rHit, distance))
+        {
+            if (rHit.collider.tag== "Enemy")
+            {
+                Debug.Log("hit enemy.");
+            }
 
-        if (hit.collider != null)
-        {
-            Debug.Log("testing collider hit");
-            Debug.DrawLine(firePointPosition, hit.point, Color.red);
-            Debug.Log("We Hit " + hit.collider.name + " and did " + damage + " damage.");
+            if(rHit.collider.tag == "Platform")
+            {
+                Debug.Log("Hit Platform.");
+            }
         }
+    }
+
+    void Effect()
+    {
+        // create quaternion based on mousePosition
+        Vector3 mousePosZ = Input.mousePosition;
+        mousePosZ.z = 10;
+        Vector3 difference = Camera.main.ScreenToWorldPoint(mousePosZ) - transform.position;
+        difference.Normalize();
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        Quaternion newQuat = Quaternion.Euler(0f, 0f, rotZ +0);
+       
+        // Create Bullet Trail
+        Instantiate(BulletTrailPrefab, firePoint.position, newQuat);
+        // Create Muzzle Flash
+        Transform clone = (Transform)Instantiate(MuzzleFlashPrefab, firePoint.position, newQuat);
+        clone.parent = firePoint;
+        //float size = Random.Range(0.9f, 1.3f);
+        //clone.localScale = new Vector3(size, size, 9.58f);
+        Destroy(clone.gameObject, 0.5f);
     }
 }
