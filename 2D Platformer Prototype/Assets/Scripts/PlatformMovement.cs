1245f9ee3755da7ef6d9a5f17ef955d7d6edbb54
Nicholas Sparks <nsparks86@gmail.com>
2015-07-27 14:22:02 +0300

    Fixed Platform Movement

--- 2D Platformer Prototype/Assets/Scripts/PlatformMovement.cs
+++ 2D Platformer Prototype/Assets/Scripts/PlatformMovement.cs
@@ -15,21 +15,21 @@ public class PlatformMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (type == 0) {
-			if (transform.position.x >= startPosition.x + range){
+			if (transform.position.x >= startPosition.x + range - 0.1){
 				nextPosition = new Vector3 (startPosition.x, startPosition.y, startPosition.z);
 				xDirection = -1;
 			}
-			else if (transform.position.x <= startPosition.x) {
+			else if (transform.position.x <= startPosition.x + 0.1) {
 				nextPosition = new Vector3 (startPosition.x + range, startPosition.y, startPosition.z);
 				xDirection = 1;
 			}
 		}
 		if (type == 1) {
-			if (transform.position.y >= startPosition.y + range){
+			if (transform.position.y >= startPosition.y + range - 0.1){
 				nextPosition = new Vector3(startPosition.x, startPosition.y, startPosition.z);
 				yDirection = -1;
 			}
-			else if (transform.position.y <= startPosition.y){
+			else if (transform.position.y <= startPosition.y + 0.1){
 				nextPosition = new Vector3(startPosition.x, startPosition.y + range, startPosition.z);
 				yDirection = 1;
 			}
